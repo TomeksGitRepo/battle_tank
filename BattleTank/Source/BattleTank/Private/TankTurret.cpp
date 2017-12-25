48afcbe3e14edc51300d1f5f506685800781b9d9
tomek <tomek7sa@gmail.com>
2017-12-25 14:19:13 +0100

    Turret rotate to aiming point.

--- BattleTank/Source/BattleTank/Private/TankTurret.cpp
+++ BattleTank/Source/BattleTank/Private/TankTurret.cpp
@@ -5,7 +5,16 @@
 #include "TankAimingComponent.h"
 
 
+void UTankTurret::Rotate(float RelativeSpeed)
+{
+	//Move the Turret the right amount this frame
+	//Given a max elevation speed, and frame time
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto RotationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto Rotation = RelativeRotation.Yaw + RotationChange; 
 
+	SetRelativeRotation(FRotator(0, Rotation, 0));
+}
 
 
 
