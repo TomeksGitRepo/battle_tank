fa227261dd86c67a40f69c519eab32f691ed5b23
tomek <tomek7sa@gmail.com>
2017-12-25 09:58:04 +0100

    Taking tick from unnecessery components

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -2,14 +2,17 @@
 
 #include "TankBarrel.h"
 #include "BattleTank.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
 
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	//Move the barrel the right amount this frame
 	//Given a max elevation speed, and frame time
 
-	UE_LOG(LogTemp, Warning, TEXT("Barel->Elevate() called at speed %f "), DegreesPerSecond);
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f: Barel->Elevate() called at speed %f "), Time, RelativeSpeed);
 }
 
 
